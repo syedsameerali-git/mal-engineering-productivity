61cafc9d0233a9327cbea0833732d5a65f94582c
Syed Sameer Ali <syedsameer.huda@gmail.com>
2026-08-12 15:26:11 +0400

    Initial Spring Boot setup

---
+++ src/main/java/mal_productivity/MalProductivityApplication.java
@@ -0,0 +1,13 @@
+package mal_productivity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MalProductivityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MalProductivityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mal_productivity/MalProductivityApplicationTests.java
@@ -0,0 +1,13 @@
+package mal_productivity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MalProductivityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
